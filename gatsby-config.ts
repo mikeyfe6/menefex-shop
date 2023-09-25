2f9c8a6195a8e5793d7c40910bdac1ab3100d9b5
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2023-09-25 21:00:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,33 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Menefex Shop`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-shopify',
+    options: {
+      "shopName": "Menefex Shop",
+      "accessToken": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
